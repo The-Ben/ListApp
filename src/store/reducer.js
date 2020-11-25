a6c8aad2f2729b3e2523c4f880b2e11b14a47195
Ben Wakutz <the.ben.wakutz@gmail.com>
2020-11-25 17:45:07 -0500

    Re-Add 2

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,54 @@
-export const actionCreators = {
-    addToList: data => ({ type: "ADD_TO_LIST", payload: data }),
-    addToBasket: data => ({ type: "ADD_TO_BASKET", payload: data }),
-    removeItem: data => ({ type: "REMOVE_ITEM", payload: data }),
-    clearItems: () => ({ type: "CLEAR_ITEMS" })
-  };
+const initialState = {
+    items: []
+}
+
+export default (state = initialState, action) =>{
+    switch (action.type){
+        case "ADD_TO_LIST":
+            return{
+                ...state,
+                items: [
+                    ...state.items,
+                    {
+                        value: action.payload,
+                        inBasket: false
+                    }
+                ]
+            };
+        case "CLEAR_ITEMS": {
+            return{
+                items:[]
+            };
+        }
+        case "ADD_TO _BASKET":
+            return{
+                ...state,
+                items: updateObjectInArray(state.items, action)
+            };
+        case "REMOVE_ITEM":
+            return{
+                ...state,
+                items: removeItemFromList(state.items,action)
+            };
+            default: 
+            return state;
+    }
+}
+
+function updateObjectInArray(array, action){
+    return array.map((item, index)=>{
+        if(index !== action.index){
+            //Item we keep as is
+            return item
+        }
+        //item we want - return updated value
+        return {
+            ...item,
+            inBasket:true
+        };
+    });
+};
+
+function removeItemFromList(array, action){
+    return array.filter((item, index)=> index !== action.index)
+}
